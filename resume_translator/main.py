5f081f4ce15c48bb813ba7e319c5b4dee2cfcb5f
tomer-klaiman <80051196+tomer-klaiman@users.noreply.github.com>
2023-03-02 23:35:12 +0200

    first parser

---
+++ resume_translator/main.py
@@ -0,0 +1,53 @@
+import PyPDF2
+import pdfminer.high_level as pdfminer
+import re
+
+
+def extract_text_from_pdf_pypdf(pdf_file: str) -> [str]:
+    # Open the PDF file of your choice
+    with open(pdf_file, 'rb') as pdf:
+        reader = PyPDF2.PdfReader(pdf, strict=False)
+        # no_pages = len(reader.pages)
+
+
+        for page in reader.pages:
+            return(page.extract_text())
+    return "hi"
+
+
+
+def extract_text_from_pdf_pdfminer(pdf_file: str) -> [str]:
+    # Open the PDF file of your choice
+    text = pdfminer.extract_text(pdf_file)
+
+    return text
+
+def notFlip(string):
+    pattern = re.compile(r'[0-9a-zA-Z@]')
+    return bool(pattern.search(string))
+
+
+
+def main():
+    FILENAME = 'example.pdf'
+
+    #extracted_text = extract_text_from_pdf_pypdf(FILENAME)
+    extracted_text = extract_text_from_pdf_pdfminer(FILENAME)
+    textArr = extracted_text.split("\n")
+
+    for i in range(len(textArr)):
+            textArr[i]= textArr[i][::-1]
+            wordArr = textArr[i].split()
+            for j in range(len(wordArr)):
+                if notFlip(wordArr[j]):
+                    wordArr[j]= wordArr[j][::-1]
+            textArr[i]= " ".join(wordArr)
+
+
+    for word in textArr:
+        # split_message = re.split(r'\s+|[,;?!.-]\s*', text.lower())
+        print(word)
+
+
+if __name__ == '__main__':
+    main()
